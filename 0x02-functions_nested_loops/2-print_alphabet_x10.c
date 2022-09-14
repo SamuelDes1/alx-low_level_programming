9225548e2fd6bcdcdce29a1b129e029cef70b7d6
SamuelDes1 <samueldesmond653@gmail.com>
2022-09-14 11:27:26 -0700

    fix

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_alphabet*10 - check the code
+ * print_alphabet_x10 - check the code
  *
  * Return: Always 0.
  */
@@ -8,7 +8,7 @@ void print_alphabet_x10(void)
 {
 int integer = 1;
 
-while(integer <= 10)
+while (integer <= 10)
 {
 char alphabet = 'a';
 
