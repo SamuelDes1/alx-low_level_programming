f856bba31b797c404521e378250eda4602dcbd7d
SamuelDes1 <samueldesmond653@gmail.com>
2022-09-14 10:18:35 -0700

    1-print_alphabet.c

---
+++ 0x02-functions_nested_loops/1-print_alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet - check the code
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+char alphabet = 'a';
+
+while (alphabet <= 'z')
+{
+_putchar(alphabet);
+alphabet++;
+}
+_putchar('\n');
+}
