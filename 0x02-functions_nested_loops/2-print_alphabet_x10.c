cb8e056ea5f773fb1acc920397dd3a5244af5a13
SamuelDes1 <samueldesmond653@gmail.com>
2022-09-14 10:46:22 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main h"
+/**
+ * print_alphabet*10 - check the code
+ *
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void)
+{
+int integer = 1;
+
+while(integer <= 10)
+}
+_putchar(alphabet);
+
+alphabet++;
+
+}
+
+_putchar('\n');
+
+}
+
+
+
+
